ab46f5d12665ef12ea68cf02b410245f7cfe461c
Krishna Pratap Singh <kpsingh1234@gmail.com>
2024-04-05 09:54:41 +0530

    Adapter - Social Media

--- src/com/lld/design/patterns/adapter/socialmedia/manager/SocialMediaManager.java
+++ src/com/lld/design/patterns/adapter/socialmedia/manager/SocialMediaManager.java
@@ -1,21 +1,22 @@
 package com.lld.design.patterns.adapter.socialmedia.manager;
 
-import com.lld.design.patterns.adapter.socialmedia.external.FacebookApi;
-import com.lld.design.patterns.adapter.socialmedia.external.FacebookPost;
-import com.lld.design.patterns.adapter.socialmedia.external.TwitterApi;
-import com.lld.design.patterns.adapter.socialmedia.external.TwitterTweet;
+import com.lld.design.patterns.adapter.socialmedia.adapter.FacebookAdapter;
+import com.lld.design.patterns.adapter.socialmedia.adapter.SocialMediaAdapter;
+import com.lld.design.patterns.adapter.socialmedia.adapter.SocialMediaPost;
+import com.lld.design.patterns.adapter.socialmedia.adapter.TwitterAdapter;
 
 import java.util.List;
 
 public class SocialMediaManager {
-    private FacebookApi facebookApi = new FacebookApi();
-    private TwitterApi twitterApi = new TwitterApi();
+    private SocialMediaAdapter adapterFacebook = new FacebookAdapter();
+    private SocialMediaAdapter adapterTwitter = new TwitterAdapter();
 
     public void getMessages(Long userId, Long timestamp, String platform) {
         if (platform.equals("facebook")) {
-            List<FacebookPost> posts = facebookApi.fetchFacebookPosts(userId, timestamp);
+            List<SocialMediaPost> posts = adapterFacebook.getPosts(userId, timestamp);
+
         } else if (platform.equals("twitter")) {
-            List<TwitterTweet> tweets = twitterApi.getTweets(userId);
+            List<SocialMediaPost> posts = adapterTwitter.getPosts(userId, timestamp);
         }
 
         // Convert the posts/tweets to a common format
