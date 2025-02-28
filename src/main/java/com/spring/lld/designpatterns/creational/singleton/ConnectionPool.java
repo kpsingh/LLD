36bf02574ec3585823c80776fc6170e2a737f6c7
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-02-28 11:51:06 +0530

    Singleton Implementation

---
+++ src/main/java/com/spring/lld/designpatterns/creational/singleton/ConnectionPool.java
@@ -0,0 +1,31 @@
+package com.spring.lld.designpatterns.creational.singleton;
+
+public class ConnectionPool {
+    // step 1 : initialize static member
+    private static ConnectionPool INSTANCE = null; // Eager initialization - slow startup
+
+    // step 2 : Male the private constructor
+    private ConnectionPool() {
+    }
+
+    // step 3 : make the public static method to return the instance
+    public static ConnectionPool getInstance() {
+        if (INSTANCE == null) {
+            synchronized (ConnectionPool.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ConnectionPool();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+
+    // Cons of this approach:
+    // 1. SRP violation
+    // 2. Thread safety is not guaranteed
+    // 3. Performance is not guaranteed in thread-safe mode - Double checked locking
+    // Enum implementation, inner static class
+
+}
---
+++ src/test/java/com/spring/lld/LldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
